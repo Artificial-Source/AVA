2eeab51ed19c483918f8e749a3ee5806c6d34014
Carlo Wood <carlo@alinoe.com>
2026-09-18 14:20:56 +0200

    Add a loop around the unbuffered character reads of linux02-screen-resizing.cpp, so it can be used to test different terminal modes.

--- src/experimental/ncurses-tests/linux02-screen-resizing.cpp
+++ src/experimental/ncurses-tests/linux02-screen-resizing.cpp
@@ -54,48 +54,55 @@ int main()
   noecho();
   addstr("Testing with cbreak/noecho - type q - to stop\n");
 
-  //---------------------------------------------------------------------------
-  // From https://man7.org/linux/man-pages/man3/curs_getch.3x.html:
-  //
-  // wgetch returns KEY_RESIZE, even if the window's keypad mode is disabled,
-  // if ncurses has handled a SIGWINCH signal since wgetch was called;
-  // see initscr(3X) and resizeterm(3X).
-  // [...]
-  // Except for the special case KEY_RESIZE, it is necessary to enable keypad for getch to return these codes.
-
-  // keypad(stdscr, TRUE);       // Not necessary for KEY_RESIZE.
-
-  // In cbreak mode we receive one character at a time, including KEY_RESIZE.
-  int ch = 0;
-  do
+  for (int cbreak_or_raw = 0; cbreak_or_raw != 2; ++cbreak_or_raw)
   {
-    std::ostringstream text;
-    if (ch != 0)
+    //---------------------------------------------------------------------------
+    // From https://man7.org/linux/man-pages/man3/curs_getch.3x.html:
+    //
+    // wgetch returns KEY_RESIZE, even if the window's keypad mode is disabled,
+    // if ncurses has handled a SIGWINCH signal since wgetch was called;
+    // see initscr(3X) and resizeterm(3X).
+    // [...]
+    // Except for the special case KEY_RESIZE, it is necessary to enable keypad for getch to return these codes.
+
+    // keypad(stdscr, TRUE);       // Not necessary for KEY_RESIZE.
+
+    // In cbreak mode we receive one character at a time, including KEY_RESIZE.
+    int ch = 0;
+    do
     {
-      text << "ch = ";
-      if (ch == KEY_RESIZE)
-        text << "KEY_RESIZE";
-      else if (std::isprint(ch))
-        text << '\'' << static_cast<char>(ch) << '\'';
+      std::ostringstream text;
+      if (ch != 0)
+      {
+        text << "ch = ";
+        if (ch == KEY_RESIZE)
+          text << "KEY_RESIZE";
+        else if (std::isprint(ch))
+          text << '\'' << static_cast<char>(ch) << '\'';
+        else
+          text << ch;
+        text << ", ";
+      }
+      if (ch == 0 || ch == KEY_RESIZE)
+      {
+        // Get current terminal size and print it.
+        getmaxyx(stdscr, rows, cols);
+        text << "cols = " << cols << ", rows = " << rows << '\n';
+      }
       else
-        text << ch;
-      text << ", ";
-    }
-    if (ch == 0 || ch == KEY_RESIZE)
-    {
-      // Get current terminal size and print it.
-      getmaxyx(stdscr, rows, cols);
-      text << "cols = " << cols << ", rows = " << rows << '\n';
-    }
-    else
-      text << '\n';
-    // Show new values on the screen.
-    addstr(text.str().c_str());
-    refresh();
-    // getch() is the same as wgetch(stdscr).
-    ch = getch();
-  } while (ch != 'q');
-  // Note that KEY_RESIZE is delivered despite the nocbreak() because it doesn't originate from the terminal.
+        text << '\n';
+      // Show new values on the screen.
+      addstr(text.str().c_str());
+      refresh();
+      // getch() is the same as wgetch(stdscr).
+      ch = getch();
+    } while (ch != 'q');
+    // Note that KEY_RESIZE is delivered despite the nocbreak() because it doesn't originate from the terminal.
+
+    //---------------------------------------------------------------------------
+    raw();
+    addstr("Testing with raw/noecho - type q - to stop\n");
+  }
 
   //---------------------------------------------------------------------------
 
